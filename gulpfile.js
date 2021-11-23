d14ed3ce71ff2a004abad896f612c4fcea9d66a5
Samuel Shumake <shumakesam@gmail.com>
2021-11-23 14:02:32 -0500

    Desktop structure of home section

--- gulpfile.js
+++ gulpfile.js
@@ -4,14 +4,14 @@ var minify = require('gulp-minify-css');
 var uglify = require('gulp-uglify');
 
 gulp.task('styles', function() {
-    gulp.src('src/scss/style.scss')
+    return gulp.src('src/scss/style.scss')
         .pipe(sass().on('error', sass.logError))
         .pipe(minify())
         .pipe(gulp.dest('./dist/css/'));
 });
 
 gulp.task('scripts', function(){
-    gulp.src('src/js/custom.js')
+    return gulp.src('src/js/custom.js')
         .pipe(uglify())
         .pipe(gulp.dest('dist/js/'));
  });
