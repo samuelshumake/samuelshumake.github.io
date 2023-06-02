6eddf3b41d9aaa2115dac7e17909449a30d6a218
Samuel Shumake <shumakesam@gmail.com>
2023-06-02 12:24:31 -0400

    create header dropdown

--- src/js/custom.js
+++ src/js/custom.js
@@ -1,8 +1,25 @@
-// Header Scroll
-window.onscroll = function (e) {
-    if (window.scrollY > 100) {
-        document.querySelector("#header").classList.add("scrolled");
-    } else if (window.scrollY == 0) {
-        document.querySelector("#header").classList.remove("scrolled");
-    }
-} 
+document.addEventListener('DOMContentLoaded', function() {
+
+    // Header Scroll
+    window.onscroll = function (e) {
+        if (window.scrollY > 100) {
+            $("#header").addClass("scrolled");
+        } else if (window.scrollY == 0) {
+            $("#header").removeClass("scrolled");
+        }
+    } 
+
+    // Hamburger menu
+    var expanded = false;
+    $("#header__hamburger").click(function() {
+        if (!expanded) {
+            $(this).addClass("expanded");
+            $("#header__dropdown").slideDown(300);
+            expanded = true;
+        } else {
+            $(this).removeClass("expanded");
+            $("#header__dropdown").slideUp(300);
+            expanded = false;
+        }
+    });
+});
